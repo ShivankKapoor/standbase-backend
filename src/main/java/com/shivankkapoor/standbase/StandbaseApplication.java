edbfe222695624f85f2161b9db1a18023ceef28e
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2026-05-23 17:55:00 -0500

    init commit

---
+++ src/main/java/com/shivankkapoor/standbase/StandbaseApplication.java
@@ -0,0 +1,13 @@
+package com.shivankkapoor.standbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandbaseApplication.class, args);
+	}
+
+}
