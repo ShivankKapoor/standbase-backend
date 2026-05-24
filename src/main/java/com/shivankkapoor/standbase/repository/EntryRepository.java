92d2480534363f1970af81408f9f16fc6793ac57
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2026-05-23 23:23:21 -0500

    Create a Entry repo

---
+++ src/main/java/com/shivankkapoor/standbase/repository/EntryRepository.java
@@ -0,0 +1,15 @@
+package com.shivankkapoor.standbase.repository;
+
+import com.shivankkapoor.standbase.model.Entry;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface EntryRepository extends CrudRepository<Entry, UUID> {
+    Entry findByUserIdAndEntryDate(UUID userId, LocalDate entryDate);
+    List<Entry> findByUserIdAndEntryDateBetweenOrderByEntryDateDesc(UUID userId, LocalDate from, LocalDate to);
+}
