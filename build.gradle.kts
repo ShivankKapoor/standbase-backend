bbe55eb7023e189698337a453d2fcaf62d5c1d0e
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2026-05-24 04:01:58 -0500

    Start working on auth service

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("io.github.cdimascio:java-dotenv:3.1.6")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
--- src/main/java/com/shivankkapoor/standbase/controller/MainController.java
+++ src/main/java/com/shivankkapoor/standbase/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.shivankkapoor.standbase.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,11 @@ import java.util.Map;
 
 @RestController
 public class MainController {
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
     @GetMapping("/")
     ResponseEntity<Map<String, String>> home() {
+        log.info("Home endpoint has been called");
         Map<String, String> resp = new LinkedHashMap<>();
         resp.put("name", "Standbase-Backend");
         resp.put("status", "Up");
