694b5b3557829a090a86315e5c8f552467968112
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2026-05-24 01:43:09 -0500

    Make find optional incase no entry found

--- src/main/java/com/shivankkapoor/standbase/repository/EntryRepository.java
+++ src/main/java/com/shivankkapoor/standbase/repository/EntryRepository.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EntryRepository extends CrudRepository<Entry, UUID> {
-    Entry findByUserIdAndEntryDate(UUID userId, LocalDate entryDate);
+    Optional<Entry> findByUserIdAndEntryDate(UUID userId, LocalDate entryDate);
     List<Entry> findByUserIdAndEntryDateBetweenOrderByEntryDateDesc(UUID userId, LocalDate from, LocalDate to);
 }
