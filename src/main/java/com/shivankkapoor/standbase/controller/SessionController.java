a8be950d1845ec4a2fe7577a6e108ac019f71e62
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2026-05-24 22:51:07 -0500

    User is able to logout from a session

--- src/main/java/com/shivankkapoor/standbase/controller/SessionController.java
+++ src/main/java/com/shivankkapoor/standbase/controller/SessionController.java
@@ -1,12 +1,12 @@
 package com.shivankkapoor.standbase.controller;
 
 import com.shivankkapoor.standbase.dto.CheckResponseDTO;
+import com.shivankkapoor.standbase.dto.ResponseDTO;
 import com.shivankkapoor.standbase.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.UUID;
@@ -28,6 +28,16 @@ public class SessionController {
         resp.setUserName(authService.getUsernameById(userId));
         resp.setStatus("ok");
         return ResponseEntity.ok(resp);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ResponseDTO> logout(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        String token = authHeader.substring(7);
+        authService.logout(token);
+        ResponseDTO response = new ResponseDTO();
+        response.setStatus("ok");
+        return ResponseEntity.ok(response);
     }
 
 }
