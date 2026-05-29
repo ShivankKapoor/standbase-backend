82d760bc63bc81ca4f71eae1694842ce96caa6fe
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2026-05-29 01:59:41 -0500

    Store Auth events in the DB

---
+++ src/main/java/com/shivankkapoor/standbase/model/AuthEvent.java
@@ -0,0 +1,25 @@
+package com.shivankkapoor.standbase.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "auth_events")
+public class AuthEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+    @Column(name = "ip_address", nullable = false)
+    private String ipAddress;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "event_type", nullable = false)
+    private AuthEventType eventType;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+}
