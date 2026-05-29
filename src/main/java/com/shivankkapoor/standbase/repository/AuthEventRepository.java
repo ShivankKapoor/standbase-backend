ef0cf7bdedec85e36b5461e00e8e7c604c7b1711
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2026-05-29 01:59:41 -0500

    Store Auth events in the DB

---
+++ src/main/java/com/shivankkapoor/standbase/repository/AuthEventRepository.java
@@ -0,0 +1,6 @@
+package com.shivankkapoor.standbase.repository;
+
+import com.shivankkapoor.standbase.model.AuthEvent;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthEventRepository extends CrudRepository<AuthEvent, Long> {}
---
+++ src/main/java/com/shivankkapoor/standbase/service/AuthEventService.java
@@ -0,0 +1,35 @@
+package com.shivankkapoor.standbase.service;
+
+import com.shivankkapoor.standbase.model.AuthEvent;
+import com.shivankkapoor.standbase.model.AuthEventType;
+import com.shivankkapoor.standbase.repository.AuthEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AuthEventService {
+    private static final Logger log = LoggerFactory.getLogger(AuthEventService.class);
+    private final AuthEventRepository authEventRepository;
+
+    public AuthEventService(AuthEventRepository authEventRepository) {
+        this.authEventRepository = authEventRepository;
+    }
+
+    @Async
+    public void logAuthEvent(UUID userId, String ip, AuthEventType eventType) {
+        AuthEvent authEvent = new AuthEvent();
+        authEvent.setUserId(userId);
+        authEvent.setIpAddress(ip);
+        authEvent.setEventType(eventType);
+        log.info("Logging auth event {} for user {} from {}", eventType, userId, ip);
+        try {
+            authEventRepository.save(authEvent);
+        } catch (Exception e) {
+            log.error("Unable to save auth event", e);
+        }
+    }
+}
