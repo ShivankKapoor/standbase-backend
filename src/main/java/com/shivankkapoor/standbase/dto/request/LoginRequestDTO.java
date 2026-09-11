16429084643d96cf5eacea40be49d9f354e442c4
Shivank Kapoor <shivankkapoor@outlook.com>
2026-09-11 03:15:42 -0500

    Cap auth request field lengths

--- src/main/java/com/shivankkapoor/standbase/dto/request/LoginRequestDTO.java
+++ src/main/java/com/shivankkapoor/standbase/dto/request/LoginRequestDTO.java
@@ -1,12 +1,15 @@
 package com.shivankkapoor.standbase.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequestDTO {
     @NotBlank
+    @Size(max = 255)
     private String username;
     @NotBlank
+    @Size(max = 128)
     private String password;
 }
--- src/main/java/com/shivankkapoor/standbase/dto/request/TotpVerifyRequestDTO.java
+++ src/main/java/com/shivankkapoor/standbase/dto/request/TotpVerifyRequestDTO.java
@@ -1,12 +1,15 @@
 package com.shivankkapoor.standbase.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class TotpVerifyRequestDTO {
     @NotBlank
+    @Size(max = 128)
     private String preAuthToken;
     @NotBlank
+    @Size(max = 16)
     private String totpCode;
 }
